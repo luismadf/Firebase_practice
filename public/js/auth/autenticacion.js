3d2276be609974776ec29a14a4db8ea787b2040e
Luis De Freites <luisdefreites120@gmail.com>
2021-01-03 20:09:42 +0100

    Second commit

--- public/js/auth/autenticacion.js
+++ public/js/auth/autenticacion.js
@@ -1,34 +1,46 @@
 class Autenticacion {
-  autEmailPass (email, password) {
+  autEmailPass(email, password) {
     //$('#avatar').attr('src', 'imagenes/usuario_auth.png')
     //Materialize.toast(`Bienvenido ${result.user.displayName}`, 5000)
     //$('.modal').modal('close')
-   
   }
 
-  crearCuentaEmailPass (email, password, nombres) {
+  crearCuentaEmailPass(email, password, nombres) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPasword(email, password)
+      .then((result) => {
+        result.user.updateProfile({
+          displayName: nombres,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+        Materialize.toast(error.message, 4000);
+      });
+
     /*Materialize.toast(
+      
       `Bienvenido ${nombres}, debes realizar el proceso de verificación`,
       4000
     )
 
     $('.modal').modal('close')*/
-    
   }
 
-  authCuentaGoogle () {
+  authCuentaGoogle() {
     //$('#avatar').attr('src', result.user.photoURL)
     //$('.modal').modal('close')
     //Materialize.toast(`Bienvenido ${result.user.displayName} !! `, 4000)
   }
 
-  authCuentaFacebook () {
+  authCuentaFacebook() {
     //$('#avatar').attr('src', result.user.photoURL)
     //$('.modal').modal('close')
     //Materialize.toast(`Bienvenido ${result.user.displayName} !! `, 4000)
   }
 
-  authTwitter () {
+  authTwitter() {
     // TODO: Crear auth con twitter
   }
 }
